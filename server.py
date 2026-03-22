b16fbbbf2797cd5c1be0ff76984825386414eea5
Sahil Behera <sahilbehera111333@gmail.com>
2026-03-22 18:39:55 +0530

    feat: integrate groq scoring into ask endpoint

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
+from gen import calculate_accuracyscore
 
 app = Flask(__name__)
+prev_question = "What is Data Science?"
 
 @app.route('/')
 def home():
@@ -16,12 +18,17 @@ def group_discussion():
 
 @app.route('/ask', methods=['POST'])
 def ask_question():
+    global prev_question
+
     data = request.json
     answer = data.get('answer', '')
 
+    accuracy_score = calculate_accuracyscore(answer, prev_question)
+
+
     return jsonify({
-        "accuracy_score": 50,
-        "next_question": "What is a linked list?"
+        "accuracy_score": accuracy_score,
+        "next_question": prev_question
     })
 
 if __name__ == '__main__':
